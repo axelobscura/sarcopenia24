e541ca85e59e4a3c6f7fa1ea760a81397602e4d4
Axel Obscura <axelobscura@Axels-MacBook-Pro.local>
2020-08-12 16:40:56 -0500

    dashboard

--- pages/administrador.js
+++ pages/administrador.js
@@ -5,24 +5,44 @@ export default function Administrador() {
 
     return (
         <section id="administrador" className="section-bg wow fadeInUp m-0 p-0" style={{ paddingTop: '200px' }}>
-            <div className="container-fluid">
+            <div className="container-fluid" style={{background: '#fff'}}>
                 <div className="row">
                     <div className="col-md-12 text-center">
-                        <h1>HOLA</h1>
-                        <img src="img/logo-amgg.png" alt="" style={{ width: '200px'}} />
+                        <img src="images/logo-amgg.png" alt="" style={{ width: '350px', padding: '20px'}} />
                     </div>
                 </div>
             </div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-md-6 p-0">
                         <div className="tarjeta">
+                            <div className="bkg"></div>
                             <h1>AMGG TV</h1>
                             <img src="https://images.pexels.com/photos/3758756/pexels-photo-3758756.jpeg" className="img-fluid" alt="" />
                         </div>
                     </div>
-                    <div className="col-md-6">
-                        <Calendario />
+                    <div className="col-md-6 p-0">
+                        <div className="tarjeta">
+                            <div className="bkg"></div>
+                            <h1>PONENCIAS</h1>
+                            <img src="https://images.pexels.com/photos/3082452/pexels-photo-3082452.jpeg" className="img-fluid" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 p-0">
+                        <div className="tarjeta">
+                            <div className="bkg"></div>
+                            <h1>ALIADOS ESTRATÉGICOS</h1>
+                            <img src="https://images.pexels.com/photos/2833037/pexels-photo-2833037.jpeg" className="img-fluid" alt="" />
+                        </div>
+                    </div>
+                    <div className="col-md-6 p-0">
+                        <div className="tarjeta">
+                            <div className="bkg"></div>
+                            <h1>PROGRAMA ACDÉMICO</h1>
+                            <img src="https://images.pexels.com/photos/273153/pexels-photo-273153.jpeg" className="img-fluid" alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
