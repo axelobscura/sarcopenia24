06c0c73fb1c4f9b1ef96a15ffc2ef421683eb374
axelobscura <axosar@gmail.com>
2020-10-31 13:52:44 -0600

    Cambio1

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <section id="intro">
         <div className="intro-container wow fadeIn">
-          <h1 className="mb-4 pb-0">Soluciones en<br /><span>WEBINARS APARA MÉDICOS</span></h1>
+          <h1 className="mb-4 pb-0">Soluciones en<br /><span>WEBINARS PARA MÉDICOS</span></h1>
           <p className="mb-4 pb-0">comunica y conecta de una manera eficaz, certera, intuitiva y oportuna.</p>
           <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" className="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
           <Link href="/registro"><a className="about-btn scrollto">LOGIN | REGISTRO</a></Link>
