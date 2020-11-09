23090eb20b2ccde60b7465ca948829faf13ca6d8
axelobscura <axosar@gmail.com>
2020-11-09 14:28:51 -0600

    Textos

--- pages/registro.js
+++ pages/registro.js
@@ -45,7 +45,7 @@ export default function Registro() {
                     <div className="col-md-6">
                         <div className="contact-address">
                             <i className="ion-ios-location-outline"></i>
-                            <h3>LOGIN DE USUARIOS | DOCTORES ASOCIADOS | ENEFERMEROS | LICENCIADOS</h3>
+                            <h3>LOGIN DE USUARIOS | DOCTORES ASOCIADOS | ENEFERMEROS | LICENCIADOS | GERONTÓLOGOS</h3>
                             {message != '' && <div className="message">{message}</div>}
                             <hr />
                             <form onSubmit={handleUsuario}>
