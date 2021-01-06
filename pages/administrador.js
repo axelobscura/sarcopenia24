62b8b44bf4e8d59fc45190cbd003583258e75312
Axel <axosar@gmail.com>
2021-01-05 18:18:23 -0600

    Menu de usuarios

--- pages/administrador.js
+++ pages/administrador.js
@@ -5,50 +5,47 @@ import Calendario from './components/Calendario';
 export default function Administrador() {
 
     return (
-        <section id="administrador" className="section-bg wow fadeInUp m-0 p-0" style={{ paddingTop: '200px' }}>
-            <div className="container-fluid" style={{background: '#fff'}}>
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                        <img src="images/logo-amgg.png" alt="" style={{ width: '350px', padding: '20px'}} />
-                    </div>
-                </div>
-            </div>
+        <section id="administrador" className="section-bg wow fadeInUp m-0 p-0">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-6 p-0">
-                        <Link href="/conferencias">
-                            <div className="tarjeta">
-                                <div className="bkg"></div>
-                                <h1>AMGG TV</h1>
-                                <img src="https://images.pexels.com/photos/3758756/pexels-photo-3758756.jpeg" className="img-fluid" alt="" />
-                            </div>
-                        </Link>
+                    <div className="col-md-3 text-center m-0 p-0">
+                        <h3>MENÚ DE USUARIO</h3>
+                        <ul>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> PERFIL DE USUARIO</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> CALENDARIO</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> CONFERENCIAS</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> SIMPOSIOS</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> CONSTANCIAS</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> AFILIACIONES</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> BIBLIOTECA DE DOCUMENTOS</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> DESCUENTOS</a></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><a><i className="fa fa-angle-right"></i> PATROCINADORES</a></Link>
+                            </li>
+                        </ul>
                     </div>
-                    <div className="col-md-6 p-0">
-                        <div className="tarjeta">
-                            <div className="bkg"></div>
-                            <h1>PONENCIAS</h1>
-                            <img src="https://images.pexels.com/photos/3082452/pexels-photo-3082452.jpeg" className="img-fluid" alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 p-0">
-                        <div className="tarjeta">
-                            <div className="bkg"></div>
-                            <h1>ALIADOS ESTRATÉGICOS</h1>
-                            <img src="https://images.pexels.com/photos/2833037/pexels-photo-2833037.jpeg" className="img-fluid" alt="" />
-                        </div>
-                    </div>
-                    <div className="col-md-6 p-0">
-                        <div className="tarjeta">
-                            <div className="bkg"></div>
-                            <h1>PROGRAMA ACADÉMICO (dividir el programa)</h1>
-                            <img src="https://images.pexels.com/photos/273153/pexels-photo-273153.jpeg" className="img-fluid" alt="" />
-                        </div>
+                    <div className="col-md-9 text-center">
+                        
                     </div>
                 </div>
             </div>
+            
             <style global jsx>{`
                 body {
                 background: #04091B;
