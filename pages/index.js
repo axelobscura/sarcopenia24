656865ea00dd580305342833e52b7b3b0a2b31ac
unknown <axosar@gmail.com>
2021-11-16 20:33:53 -0600

    ponente

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           <img src="/images/logo-congreso21.png" alt="Congreso Internacional AMGG 2021" title="Congreso Internacional AMGG 2021" style={{'width':'350px'}} />
           <h2 className="mt-3 mb-1 pb-0 text-white font-weight-regular">{status === 'success' ? '¡Se ha registrado con éxito!' : '2, 3, 4 y 5 de DICIEMBRE, 2021'}</h2>
           <p className="font-weight-bold m-0 text-white">CONSTANCIA CON VALOR CURRICULAR DE 40 HORAS</p>
-          {status === 'success' ? '¡BIENVENIDO!' : '<p className="mb-1 p-3 pt-0"><small>Dirigido a Médicos Generales, Médicos internistas, Médicos Familiares, Geriatras, Gerontólogos<br/>Nutriólogos, Estudiantes, Personal de Enfermería, Cuidadores y especialidades afines</small></p>'}
+          {status === 'success' ? <p className="mb-1 p-3 pt-0">¡BIENVENIDO!</p> : <p className="mb-1 p-3 pt-0"><small>Dirigido a Médicos Generales, Médicos internistas, Médicos Familiares, Geriatras, Gerontólogos<br/>Nutriólogos, Estudiantes, Personal de Enfermería, Cuidadores y especialidades afines</small></p>}
           {/* 
           <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" className="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
           */}
