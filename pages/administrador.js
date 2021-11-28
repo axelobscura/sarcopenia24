e1ca172d090957a4235c81d1648f777712e58cbe
unknown <axosar@gmail.com>
2021-11-28 12:25:50 -0600

    chat

--- pages/administrador.js
+++ pages/administrador.js
@@ -28,7 +28,7 @@ export default function Administrador() {
     ));
     console.log(usuario);
     return (
-        <section id="administrador" className="section-bg wow fadeInDown m-0 p-0">
+        <section id="administrador" className="section-bg wow fadeInDown m-0 p-0" style={{'paddingTop':'100px'}}>
             <div className="container-fluid">
                 <div className="row">
                     <MenuAdmin/>
@@ -47,7 +47,7 @@ export default function Administrador() {
             </div>
             <style global jsx>{`
                 body {
-                background: #04091B;
+                    background: #04091B;
                 }
             `}</style>
         </section>
--- pages/components/Chat.js
+++ pages/components/Chat.js
@@ -1,10 +1,19 @@
-import Mensaje from "./Mensaje"
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+import UserContext from "../../UserContext";
 
 export default function Chat() {
-
+    const [mensaje, setMensaje] = useState('');
+    //const { usuario } = useContext(UserContext);
+    const enviarMensaje = (e) => {
+        e.preventDefault();
+        let mensaje = e.target.mensaje.value;
+        setMensaje(mensaje);
+    }
+    console.log(mensaje);
     return (
         <div className="form-group chat">
-            <form>
+            <form onSubmit={enviarMensaje}>
                 <textarea id="mensaje" name="mensaje" rows="4" />
                 <input type="submit" value="PUBLICAR COMENTARIO" className="btn_chat" />
             </form>
