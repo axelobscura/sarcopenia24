da4a768207627e9f42ee9797ccfaae4019b8008e
Axel <axosar@gmail.com>
2021-11-30 17:21:07 -0600

    arrows

--- pages/components/MenuAdminEvento.js
+++ pages/components/MenuAdminEvento.js
@@ -1,8 +1,14 @@
 import Link from 'next/link';
-import { PersonCircle, FileEarmark, ListCheck, FileBinaryFill, BrightnessAltHighFill } from 'react-bootstrap-icons';
+import { ListCheck } from 'react-bootstrap-icons';
 import Chat from './Chat';
+import { useRouter } from 'next/router';
+import Loader from './Loader';
 
 export default function MenuAdminEvento() {
+  const router = useRouter();
+  const { tipo } = router.query;
+
+  console.log(tipo);
   return (
     <div className="col-md-3 text-center m-0 p-0">
       <div className="organizador evento">
@@ -10,10 +16,18 @@ export default function MenuAdminEvento() {
       </div>
       <ul>
         <li>
-          <Link href="/oferta-de-cursos"><a><ListCheck className="iconoBoot" /> SALÓN 1 | GERIATRÍA</a></Link>
+          <Link href="/salon/geriatria">
+            <a style={{
+            background: tipo === 'geriatria' ? '#f82249' : ''
+            }}><ListCheck className="iconoBoot" /> SALÓN 1 | GERIATRÍA</a>
+          </Link>
         </li>
         <li>
-          <Link href="/oferta-de-cursos"><a><ListCheck className="iconoBoot" /> SALÓN 2 | GERONTOLOGÍA</a></Link>
+          <Link href="/salon/gerontologia">
+            <a style={{
+            background: tipo === 'gerontologia' ? '#f82249' : ''
+            }}><ListCheck className="iconoBoot" /> SALÓN 2 | GERONTOLOGÍA</a>
+          </Link>
         </li>
         {/* 
         <li>
--- pages/salon/[tipo].js
+++ pages/salon/[tipo].js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router';
 import MenuAdminEvento from '../components/MenuAdminEvento';
 import ReactPlayer from 'react-player';
-import Patrocinadores from '../components/Patrocinadores';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
 
-export default function Curso() {
+export default function Salon() {
   const router = useRouter();
-  const { curso } = router.query;
+  const { tipo } = router.query;
   return (
       <section id="administrador" className="section-bg wow fadeInUp m-0 p-0">
         <div className="container-fluid">
@@ -13,13 +13,14 @@ export default function Curso() {
             <MenuAdminEvento />
             <div className="col-md-9 vh-80">
               <div className="row vh-100">
+                <h2 className="tipoevento"><ArrowRightCircle /> {tipo === "geriatria" ? 'SALÓN 1 - GERIATRÍA' : 'SALÓN 2 - GERONTOLOGÍA'}</h2>
                 <ReactPlayer 
                   className='react-player'
                   url='https://www.youtube.com/watch?v=FhRKmIeRQj8gv?rel=0;autoplay=1'
                   width='100%'
                   height='100vh'
-                  loop='true'
-                  playing='true'
+                  loop={true}
+                  playing={true}
                   config={{
                     youtube: {
                       playerVars: { 
@@ -33,10 +34,12 @@ export default function Curso() {
             </div>
           </div>
         </div>
-        
         <style global jsx>{`
             body {
             background: #04091B;
+            }
+            .ytp-copylink-title {
+              display: none !important;
             }
         `}</style>
       </section>
