d3563585b549983baf4ea45415d5dd0a3c9b8a5f
Axel <axosar@gmail.com>
2021-11-30 15:47:40 -0600

    footer

--- pages/patrocinadores.js
+++ pages/patrocinadores.js
@@ -13,10 +13,20 @@ export default function Patrocinadores() {
             <div className="container-fluid">
                 <div className="row">
                     <MenuAdmin />
-                    <div className="col-md-9 patrocinadores">
+                    <div className="col-md-9 patrocinadores dos">
                         <div className="row">
                             <div className="col-md-12 mb-3">
-                                
+                                <div className="logospatro">
+                                  <img src="https://capuletbeta.com/congreso2021/images/logo-hiequilibrio.jpg" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid"/>
+                                  <img src="https://capuletbeta.com/nutricion/images/Avicola.jpeg" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="https://capuletbeta.com/nutricion2021/images/logo-abbot.png" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="https://capuletbeta.com/nutricion/images/logo-amsa.png" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="https://capuletbeta.com/nutricion/images/logo-angeles.png" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="https://capuletbeta.com/nutricion/images/Fresenius.jpeg" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="https://capuletbeta.com/nutricion/images/Fresubin.png" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="/images/logo_affective.jpg" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                  <img src="/images/logo_deasa.png" alt="CAPULET SOLUCIONES DE ADMINISTRACIÓN DE DATOS Y COMUNICACIÓN" className="img-fluid" />
+                                </div>
                             </div>
                         </div>
                     </div>
