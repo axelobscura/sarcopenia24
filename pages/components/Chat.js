a1fb90d78cbe1b5f3a22bb608a7148c1758f8fc3
unknown <axosar@gmail.com>
2021-12-01 00:05:18 -0600

    chat

--- pages/components/Chat.js
+++ pages/components/Chat.js
@@ -1,21 +1,22 @@
 import { useState, useContext, useEffect } from "react";
-import { Check2Circle } from 'react-bootstrap-icons';
 import Mensaje from "./Mensaje";
 import UserContext from "../../UserContext";
 import Loader from './Loader';
 
 import { useMensajes } from '../../lib/swr-hooks';
 
-export default function Chat({tipo}) {
+export default function Chat({ posts, tipo }) {
     const [mensajelocal, setMensajelocal] = useState('');
-    const { mensajes, isLoadingMensajes } = useMensajes();
+    let { mensajes, isLoadingMensajes } = useMensajes();
     const { usuario } = useContext(UserContext);
+
+    console.log('Los posts: '+posts);
 
     async function submitHandler(e) {
         e.preventDefault();
         let elusuario = e.target.usuario.value;
         let elmensaje = e.target.mensaje.value;
-        let seccion = "general";
+        let seccion = tipo;
         try {
           const res = await fetch('/api/create-entry', {
             method: 'POST',
@@ -29,25 +30,24 @@ export default function Chat({tipo}) {
             }),
           })
           const json = await res.json();
-          if (!res.ok) throw Error(json.message);
-          useMensajes();
+          if (!res.ok) throw Error(json.message)
         } catch (e) {
           throw Error(e.message)
         }
     }
 
-    if(isLoadingMensajes && !mensajes){
+    if (isLoadingMensajes && !mensajes) {
         return(
-            <Loader />
+            <Loader/>
         )
-    }
+    };
     
     return (
         <div className="form-group chat">
             <form onSubmit={submitHandler}>
                 <input type="hidden" name="usuario" value={usuario} />
                 <textarea id="mensaje" name="mensaje" rows="4" placeholder="Escriba su mensaje aquí..." />
-                <button type="submit" className={`btn_chat ${tipo}`}><Check2Circle className="mr-2"/>DAR CLICK AQUÍ PARA PUBLICAR COMENTARIO</button>
+                <input type="submit" value="PUBLICAR COMENTARIO" className="btn_chat" />
             </form>
             <div className="chat-container">
                 {mensajes.map((msj, i) => (
@@ -63,3 +63,15 @@ export default function Chat({tipo}) {
         </div>
     )
 }
+
+export async function getServerSideProps() {
+    const res = await fetch('/api/create-entry')
+    const posts = await res.json()
+  
+    return {
+      props: {
+        posts,
+      },
+      revalidate: 1,
+    }
+  }
