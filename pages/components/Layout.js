79d7ecd8f5e340159a729eb64789e3364830efdb
Axel <axosar@gmail.com>
2021-12-01 19:33:31 -0600

    analitycs

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -23,14 +23,13 @@ class Layout extends Component {
                         async
                         src="https://www.googletagmanager.com/gtag/js?id=G-9651GPZCV5"
                     />
-
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
-                            gtag('config', 'G-9651GPZCV5', { page_path: window.location.pathname });
+                            gtag('config', 'G-9651GPZCV5');
                             `,
                         }}
                     />
